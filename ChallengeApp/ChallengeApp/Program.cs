10669b5f4e72c4f15a4d3b84629591676b176898
Albert Baran <a.baran20@wp.pl>
2023-11-27 21:40:30 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string gender = "woman";
+int myAge = 33;
+
+if (gender =="woman" && myAge <30)
+{
+    Console.WriteLine("(1)- Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && myAge == 33)
+{
+    Console.WriteLine("(2)- Ewa lat 33");
+}
+else if (gender == "man" && myAge < 18)
+{
+    Console.WriteLine("(3)- Niepełnoletni mężczyzna");
+}
